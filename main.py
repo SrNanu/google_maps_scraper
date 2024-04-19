b5724397c46a9d8767f2c1fda02b6d0e9f49a9fb
SrNanu <107449931+SrNanu@users.noreply.github.com>
2024-04-19 11:11:15 -0300

    Correcion errores, busqueda por ciudad, un solo excel con cel formateado

--- main.py
+++ main.py
@@ -48,7 +48,21 @@ class BusinessList:
 
         if not os.path.exists(self.save_at):
             os.makedirs(self.save_at)
-        self.dataframe().to_excel(f"output/{filename}.xlsx", index=False)
+        try:
+            self.dataframe().to_excel(f"output/{filename}.xlsx", index=False) #nanu cambio
+            print("Bien a la 1")
+
+        except:
+            try:
+                self.dataframe().to_excel(f"output\{filename}.xlsx", index=False)
+                print("Bien a la 2")
+            except:
+                try:
+                    self.dataframe().to_excel(f"{filename}.xlsx", index=False)
+                    print("Bien a la 3")
+                except:
+                    print("todo mal")
+                    
 
     def save_to_csv(self, filename):
         """saves pandas dataframe to csv file
@@ -82,7 +96,7 @@ def main():
     
     if args.search:
         search_list = [args.search]
-        
+    
     if args.total:
         total = args.total
     else:
@@ -101,6 +115,12 @@ def main():
             with open(input_file_path, 'r') as file:
             # Read all lines into a list
                 search_list = file.readlines()
+        #modificado por nanu
+        search_list = [elemento.rstrip('\n') for elemento in search_list]
+
+        print(search_list)
+
+        #termina modificado por nanu        
                 
         if len(search_list) == 0:
             print('Error occured: You must either pass the -s search argument, or add searches to input.txt')
@@ -113,18 +133,32 @@ def main():
         browser = p.chromium.launch(headless=False)
         page = browser.new_page()
 
-        page.goto("https://www.google.com/maps", timeout=60000)
+        #modificado nanu
+        # lo que habia antes= page.goto("https://www.google.com/maps", timeout=60000) 
+        page.goto("https://www.google.com/maps/search/hotel/@-49.3091202,-67.7351463,15z?entry=ttu", timeout=60000)
+        #Se deben poner los enlaces con el posicionamiento de las ciudades
+        enlaces = [
+            "https://www.google.com/maps/search/hotel/@-49.3091202,-67.7351463,15z?entry=ttu",
+            "https://www.google.com/maps/search/hotel/@-54.5318911,-67.2032165,14z/data=!4m2!2m1!6e3?entry=ttu"]
+        #termina la modificacion nanu
+
         # wait is added for dev phase. can remove it in production
-        page.wait_for_timeout(5000)
-        
+        page.wait_for_timeout(2000)
+        #modificado nanu
+        business_list = BusinessList()
+        #termina modificado nanu
+
         for search_for_index, search_for in enumerate(search_list):
             print(f"-----\n{search_for_index} - {search_for}".strip())
-
+            #modificado nanu
+            #en cada iteracion se cambia de ciudad en la lista
+            page.goto(enlaces[search_for_index], timeout=60000)
+            #termina la modificacion nanu
             page.locator('//input[@id="searchboxinput"]').fill(search_for)
-            page.wait_for_timeout(3000)
+            page.wait_for_timeout(2000)
 
             page.keyboard.press("Enter")
-            page.wait_for_timeout(5000)
+            page.wait_for_timeout(2000)
 
             # scrolling
             page.hover('//a[contains(@href, "https://www.google.com/maps/place")]')
@@ -134,7 +168,7 @@ def main():
             previously_counted = 0
             while True:
                 page.mouse.wheel(0, 10000)
-                page.wait_for_timeout(3000)
+                page.wait_for_timeout(1000)
 
                 if (
                     page.locator(
@@ -173,13 +207,13 @@ def main():
                             ).count(),
                         )
 
-            business_list = BusinessList()
+            #business_list = BusinessList() modificado nanu
 
             # scraping
             for listing in listings:
                 try:
                     listing.click()
-                    page.wait_for_timeout(5000)
+                    page.wait_for_timeout(1000)
 
                     name_attibute = 'aria-label'
                     address_xpath = '//button[@data-item-id="address"]//div[contains(@class, "fontBodyMedium")]'
@@ -206,6 +240,13 @@ def main():
                         business.website = ""
                     if page.locator(phone_number_xpath).count() > 0:
                         business.phone_number = page.locator(phone_number_xpath).all()[0].inner_text()
+                        #editado nanu
+                        #se formatea el numero 
+                        business.phone_number = business.phone_number.replace(' ','')
+                        business.phone_number = business.phone_number.replace('-','')
+                        if(business.phone_number[0]=='0'):
+                            business.phone_number= business.phone_number[1:]
+                        #termina editado nanu
                     else:
                         business.phone_number = ""
                     if page.locator(review_count_xpath).count() > 0:
@@ -237,8 +278,8 @@ def main():
             #########
             # output
             #########
-            business_list.save_to_excel(f"google_maps_data_{search_for}".replace(' ', '_'))
-            business_list.save_to_csv(f"google_maps_data_{search_for}".replace(' ', '_'))
+        business_list.save_to_excel(f"google_maps_data_{search_for}_{search_for_index}".replace(' ', '_'))
+        business_list.save_to_csv(f"google_maps_data_{search_for}_{search_for_index}".replace(' ', '_'))
 
         browser.close()
 
